01d2409ea9828e056aafd793eb12821024cc8a22
omar_fouda <omarfouda@aucegypt.edu>
2023-02-13 20:55:05 +0200

    Header File

---
+++ NQueen.h
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+#define N 8
+
+void printBoard(int board[N][N]);
+
+bool isValid(int board[N][N], int row, int col);
+
+bool solveNQueen(int board[N][N], int col);
+
+bool checkSolution();
